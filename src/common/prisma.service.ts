1289394cbbee3061d051c3794842c12423a30fc6
Ilmi Nur Azis Aqli Mubarok <ilmimubarok21@gmail.com>
2024-10-12 14:57:08 +0700

    feat(config): setup winston logger, PrismaService, and ValidationService

---
+++ src/common/prisma.service.ts
@@ -0,0 +1,42 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, string>
+  implements OnModuleInit
+{
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'info',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+
+  onModuleInit() {
+    this.$on('info', this.logger.info);
+    this.$on('warn', this.logger.warn);
+    this.$on('error', this.logger.error);
+    this.$on('query', this.logger.info);
+  }
+}

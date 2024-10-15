f544323b48409a76f0e9927b6e41d1e75f2b1224
Ilmi Nur Azis Aqli Mubarok <ilmimubarok21@gmail.com>
2024-10-15 13:54:09 +0700

    feat(contact): add remove contact by id feature

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -48,5 +49,14 @@ export class ContactController {
     request.id = contactId;
     const result = await this.contactService.update(user, request);
     return { data: result };
+  }
+
+  @Delete('/:contactId')
+  async remove(
+    @Auth() user: User,
+    @Param('contactId', ParseIntPipe) contactId: number,
+  ): Promise<WebResponse<boolean>> {
+    await this.contactService.remove(user, contactId);
+    return { data: true };
   }
 }
--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -77,6 +77,19 @@ export class ContactService {
     return this.toContactResponse(contact);
   }
 
+  async remove(user: User, contactId: number): Promise<ContactResponse> {
+    await this.checkContactMustExists(user.username, contactId);
+
+    const contact = await this.prismaService.contact.delete({
+      where: {
+        id: contactId,
+        username: user.username,
+      },
+    });
+
+    return this.toContactResponse(contact);
+  }
+
   async checkContactMustExists(
     username: string,
     contactId: number,
